e6ffe687d5827a8e2ae5084055987aff3f9916af
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-01 21:38:44 +0100

    add day 1

--- day1/day1.test.ts
+++ day1/day1.test.ts
@@ -1,9 +1,24 @@
 import { readFile } from '../utilities/parser';
-import fixReport from './day1';
+import { fixReportPartOne, fixReportPartTwo } from './day1';
 
 describe('day one', () => {
   it('calculates the test case', () => {
     const expenses: number[] = readFile(`${__dirname}/fixtures/day1_test_case.txt`).map(value => parseInt(value));
-    expect(fixReport(expenses)).toEqual(514579);
+    expect(fixReportPartOne(expenses)).toEqual(514579);
+  });
+
+  it('calculates the multiplication of the tuple with size 3', () => {
+    const expenses: number[] = readFile(`${__dirname}/fixtures/day1_test_case.txt`).map(value => parseInt(value));
+    expect(fixReportPartTwo(expenses)).toEqual(241861950);
+  });
+
+  it('calculates the multiplication of the tuple with size 2', () => {
+    const expenses: number[] = readFile(`${__dirname}/fixtures/day1_input.txt`).map(value => parseInt(value));
+    expect(fixReportPartOne(expenses)).toEqual(805731);
+  });
+
+  it('calculates the multiplication of the tuple with size 3', () => {
+    const expenses: number[] = readFile(`${__dirname}/fixtures/day1_input.txt`).map(value => parseInt(value));
+    expect(fixReportPartTwo(expenses)).toEqual(192684960);
   });
 });
