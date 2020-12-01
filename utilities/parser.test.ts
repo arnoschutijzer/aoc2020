89ba4654a88cebd1749329fb1f442b518b7aa975
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-01 23:57:03 +0100

    remove typescript stuff

--- utilities/parser.test.ts
+++
@@ -1,7 +0,0 @@
-import { readFile } from './parser'
-
-describe('parser', () => {
-  it('parses some_file.txt', () => {
-    expect(readFile(`${__dirname}/fixtures/some_file.txt`)).toMatchSnapshot();
-  });
-});
--- utilities/parser.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export function readFile(filePath: string): string[] {
-  const contents = fs.readFileSync(path.resolve(filePath)).toString();
-  return contents.split("\n");
-}
