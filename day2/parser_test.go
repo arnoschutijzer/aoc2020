77b4887893337ad01c922ef5a54351f64113f06d
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-02 20:43:00 +0100

    remove redundant types

--- day2/parser_test.go
+++ day2/parser_test.go
@@ -10,19 +10,19 @@ func TestParser(t *testing.T) {
 	passwords := ReadFile("./fixtures/parser_test_1.txt")
 
 	var expected = []Password{
-		Password{
+		{
 			letter:       "a",
 			maxOccurence: 3,
 			minOccurence: 1,
 			password:     "abcde",
 		},
-		Password{
+		{
 			letter:       "b",
 			maxOccurence: 3,
 			minOccurence: 1,
 			password:     "cdefg",
 		},
-		Password{
+		{
 			letter:       "c",
 			maxOccurence: 9,
 			minOccurence: 2,
