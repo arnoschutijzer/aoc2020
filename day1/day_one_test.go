75d0731896c140f55aa6a13e252959167a409232
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-02 17:00:36 +0100

    move parser to dayone

--- day1/day_one_test.go
+++ day1/day_one_test.go
@@ -3,36 +3,35 @@ package dayone
 import (
 	"testing"
 
-	parser "arnoschutijzer.io/aoc2020/v2"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculatesMultiplicationOfPairWhichSumEquals2020(t *testing.T) {
-	expenses := parser.ReadFile("./fixtures/dayone_1.txt")
+	expenses := ReadFile("./fixtures/dayone_1.txt")
 	assert.Equal(t, 514579, fixReport(expenses, 2))
 }
 
 func TestCalculatesMultiplicationOfPairWhichSumEquals2020_2(t *testing.T) {
-	expenses := parser.ReadFile("./fixtures/dayone_personal.txt")
+	expenses := ReadFile("./fixtures/dayone_personal.txt")
 	assert.Equal(t, 805731, fixReport(expenses, 2))
 }
 
 func TestCalculatesMultiplicationOfTupleWithSize3WhichSumEquals2020(t *testing.T) {
-	expenses := parser.ReadFile("./fixtures/dayone_1.txt")
+	expenses := ReadFile("./fixtures/dayone_1.txt")
 	assert.Equal(t, 241861950, fixReport(expenses, 3))
 }
 
 func TestCalculatesMultiplicationOfTupleWithSize3WhichSumEquals2020_2(t *testing.T) {
-	expenses := parser.ReadFile("./fixtures/dayone_personal.txt")
+	expenses := ReadFile("./fixtures/dayone_personal.txt")
 	assert.Equal(t, 192684960, fixReport(expenses, 3))
 }
 
 func TestCalculatesMultiplicationOfTupleWithSize3WhichSumEquals2020WithCombinations(t *testing.T) {
-	expenses := parser.ReadFile("./fixtures/dayone_1.txt")
+	expenses := ReadFile("./fixtures/dayone_1.txt")
 	assert.Equal(t, 241861950, fixReport(expenses, 3))
 }
 
 func TestCalculatesMultiplicationOfTupleWithSize3WhichSumEquals2020WithCombinations_2(t *testing.T) {
-	expenses := parser.ReadFile("./fixtures/dayone_personal.txt")
+	expenses := ReadFile("./fixtures/dayone_personal.txt")
 	assert.Equal(t, 192684960, fixReport(expenses, 3))
 }
