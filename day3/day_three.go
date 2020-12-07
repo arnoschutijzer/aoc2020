188fecabca73167a85879bc152b77705271b5af1
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-07 20:59:27 +0100

    complete day 3, part 1

--- day3/day_three.go
+++ day3/day_three.go
@@ -1,11 +1,43 @@
 package daythree
 
-func CountEncounteredTrees(topology [][]string) int {
-	trees := 0
+type Trip struct {
+	topology         [][]string
+	encounteredTrees int
+	currentXPosition int
+	currentYPosition int
+}
 
-	if topology[1][3] == "#" {
-		trees += 1
+func NewTrip(topology [][]string) *Trip {
+	return &Trip{
+		topology:         topology,
+		currentXPosition: 0,
+		currentYPosition: 0,
+		encounteredTrees: 0,
+	}
+}
+
+func (trip *Trip) moveOn() {
+	trip.currentXPosition += 3
+	trip.currentYPosition += 1
+}
+
+func (trip *Trip) checkCurrentSpotForTrees() {
+	rowLength := trip.topology[trip.currentYPosition]
+
+	if trip.topology[trip.currentYPosition][trip.currentXPosition%len(rowLength)] == "#" {
+		trip.encounteredTrees += 1
+	}
+}
+
+func CountEncounteredTrees(topology [][]string) int {
+	trip := NewTrip(topology)
+
+	row := 0
+	for row < len(trip.topology)-1 {
+		trip.moveOn()
+		trip.checkCurrentSpotForTrees()
+		row++
 	}
 
-	return trees
+	return trip.encounteredTrees
 }
--- day3/day_three_test.go
+++ day3/day_three_test.go
@@ -6,7 +6,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCounts1TreeWith1Iteration(t *testing.T) {
+func TestTripConstructorInitializesCorrectly(t *testing.T) {
+	trip := NewTrip([][]string{})
+
+	assert.Equal(t, 0, trip.encounteredTrees)
+	assert.Equal(t, 0, trip.currentXPosition)
+	assert.Equal(t, 0, trip.currentYPosition)
+}
+
+func TestTripCorrectlyMovesOn(t *testing.T) {
+	trip := NewTrip([][]string{})
+
+	assert.Equal(t, 0, trip.currentXPosition)
+	assert.Equal(t, 0, trip.currentYPosition)
+
+	trip.moveOn()
+
+	assert.Equal(t, 3, trip.currentXPosition)
+	assert.Equal(t, 1, trip.currentYPosition)
+}
+
+func TestCounts2TreesFromFixture(t *testing.T) {
 	topology := ParseTopology("./fixtures/topology_input.txt")
-	assert.Equal(t, 1, CountEncounteredTrees(topology))
+	assert.Equal(t, 2, CountEncounteredTrees(topology))
+}
+
+func TestCountsTreesFromFixture(t *testing.T) {
+	topology := ParseTopology("./fixtures/topology_personal.txt")
+	assert.Equal(t, 228, CountEncounteredTrees(topology))
 }
