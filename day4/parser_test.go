8fe94ac56b2e76cf94b3f7677cba2eba77920c00
Arno Schutijzer <arno.schutijzer@skryv.com>
2020-12-12 21:03:29 +0100

    parse pid from input

--- day4/parser_test.go
+++ day4/parser_test.go
@@ -10,6 +10,7 @@ func TestParsesSinglePassport(t *testing.T) {
 	expected := []Passport{
 		{
 			fields: "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm",
+			pid:    "860033327",
 		},
 	}
 	assert.Equal(t, expected, ParsePassports("./fixtures/single_passport.txt"))
@@ -19,9 +20,11 @@ func TestParsesMultiplePassports(t *testing.T) {
 	expected := []Passport{
 		{
 			fields: "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm",
+			pid:    "860033327",
 		},
 		{
 			fields: "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929",
+			pid:    "028048884",
 		},
 	}
 	assert.Equal(t, expected, ParsePassports("./fixtures/two_passports.txt"))
